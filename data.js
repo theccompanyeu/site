1db851671ba90a7fdafdfee657fad38651bce58d
Theofilos Chanialakis <theofilos@fadir.com>
2025-10-18 01:18:11 +0300

    Update pdf handling

--- data.js
+++ data.js
@@ -1,5 +1,4 @@
 // Κάθε χρόνο πρόσθεσε 1 αντικείμενο εδώ και ανέβασε το PDF στον φάκελο pdfs/
 window.BALANCES = [
-    { year: 2025, file: "isologismos-2025.pdf", label: "Ισολογισμός 2025 (PDF)" },
-    { year: 2024, file: "isologismos-2024.pdf", label: "Ισολογισμός 2024 (PDF)" },
+    {year: 2024, file: "ΟΙΚΟΝΟΜΙΚΕΣ ΚΑΤΑΣΤΑΣΕΙΣ 2024.pdf", label: "ΟΙΚΟΝΟΜΙΚΕΣ ΚΑΤΑΣΤΑΣΕΙΣ 2024"},
 ];
