c54dadbf2d34c0a267ce71c359e7353a04407f0e
Theofilos Chanialakis <theofilos@fadir.com>
2025-10-18 01:01:56 +0300

    1st commit site

---
+++ data.js
@@ -0,0 +1,5 @@
+// Κάθε χρόνο πρόσθεσε 1 αντικείμενο εδώ και ανέβασε το PDF στον φάκελο pdfs/
+window.BALANCES = [
+    { year: 2025, file: "isologismos-2025.pdf", label: "Ισολογισμός 2025 (PDF)" },
+    { year: 2024, file: "isologismos-2024.pdf", label: "Ισολογισμός 2024 (PDF)" },
+];
